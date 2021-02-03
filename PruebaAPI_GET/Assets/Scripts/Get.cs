660f1c4a76b70e52412b669cb0d7eb8089341104
PyramidHead01 <hugo4444333@gmail.com>
2021-02-03 06:59:29 +0100

    21.02.03-06:58 Comentado el codigo del Get del pokemon, quizas algunas cosas estan mal explicadas, a vuestra responsabilidad queda

--- PruebaAPI_GET/Assets/Scripts/Get.cs
+++ PruebaAPI_GET/Assets/Scripts/Get.cs
@@ -1,3 +1,10 @@
+/*
+ * NOTA: El codigo no es mio, pertenece a  "Turbo Makes Games"
+ * Video: https://www.youtube.com/watch?v=GIxu8kA9EBU
+ */
+
+//Importaciones que uso
+#region Importaciones
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,31 +14,37 @@ using UnityEngine.UI;
 using TMPro;
 using System;
 using Random = UnityEngine.Random;
+#endregion
+
 
 public class Get : MonoBehaviour
 {
 
+    //Todas las variables
+    #region Variables_que_uso
+
+    //UIs para que funcione la cosa
+    #region UIs
     public RawImage SprPokemon;
     public Text NombrePokemon;
     public Text NumPokemon;
     public Text[] TipoPokemon;
+    #endregion
 
-
+    //Enlace para la Api de Pokemon
     private readonly string basePokeURL = "https://pokeapi.co/api/v2/";
 
-    void Start()
-    {
-        // A correct website page.
-        //StartCoroutine(GetRequest("https://www.example.com"));
+    #endregion
 
-        // A non-existing page.
-        //StartCoroutine(GetRequest("https://error.html"));
-    }
-
+    //Genera un numero aleatorio con todos los pokemones que existen registrados en la pagina
     public void GetRandomPokemon()
     {
+
+        //Coge el valor aleatorio
         int randomPokeIndex = Random.Range(1, 808);
 
+        //Vacio todos los datos de las UIs hasta que lleguen los datos del GET
+        #region VaciadoUIs
         SprPokemon.texture = Texture2D.blackTexture;
 
         NombrePokemon.text = "Cargando...";
@@ -42,31 +55,45 @@ public class Get : MonoBehaviour
         {
             tipo.text = "";
         }
+        #endregion
 
+        //Cargo en hilo el Get
         StartCoroutine(GetPokemon(randomPokeIndex));
 
     }
 
+    //El hilo del GET
     [System.Obsolete]
     IEnumerator GetPokemon(int numPokemon)
     {
 
+        //Tema peticiones
+        #region Peticion
+        //Modifica el URL para ponerle pokemon/{numero_en_pokedex}
         string pokemonURL = basePokeURL + "pokemon/" + numPokemon.ToString();
 
-        //Get info
-
+        //Lanza la peticion Get con la url bien formada
         UnityWebRequest pokeInfoRequest = UnityWebRequest.Get(pokemonURL);
 
+        //Se espera a que la base de datos este cargada
         yield return pokeInfoRequest.SendWebRequest();
 
+        //Comprueba que la base de datos no da ningun fallo
         if(pokeInfoRequest.isNetworkError || pokeInfoRequest.isHttpError)
         {
             Debug.LogError(pokeInfoRequest.error);
             yield break;
         }
 
+        //Obtiene el fragmento del "digimon" que nos interesa
         JSONNode pokeInfo = JSON.Parse(pokeInfoRequest.downloadHandler.text);
 
+        #endregion
+
+        //Da a cada cosa el valor que le corresponde
+        #region DarValor
+        //Todo lo referente a los textos
+        #region DarValoresTextos
         string pokeName = pokeInfo["name"];
         string pokeSpriteURL = pokeInfo["sprites"]["front_default"];
 
@@ -77,31 +104,37 @@ public class Get : MonoBehaviour
         {
             pokeTypeNames[j] = pokeTypes[i]["type"]["name"];
         }
+        #endregion
 
-        //Get sprite
+        //Todo lo referente al sprite
+        #region DarValorSprite
 
+        //Hace una peticion para el sprite
         UnityWebRequest pokeSpriteRequest = UnityWebRequestTexture.GetTexture(pokeSpriteURL);
 
+        //Espera hasta que termine
         yield return pokeSpriteRequest.SendWebRequest();
 
+        //Si falla peta
         if (pokeSpriteRequest.isNetworkError || pokeSpriteRequest.isHttpError)
         {
             Debug.LogError(pokeSpriteRequest.error);
             yield break;
         }
+        #endregion
+        #endregion
 
-        //Set UI Objects
+        //Asignar a los UIs los datos
+        #region AsignarUIs
         SprPokemon.texture = DownloadHandlerTexture.GetContent(pokeSpriteRequest);
         SprPokemon.texture.filterMode = FilterMode.Point;
 
-        //NombrePokemon.text = pokeName;
         NombrePokemon.text = CapitalizeFirstLetter(pokeName);
 
         for (int i = 0; i < TipoPokemon.Length; i++)
         {
             try
             {
-                //TipoPokemon[i].text = pokeTypeNames[i];
                 TipoPokemon[i].text = CapitalizeFirstLetter(pokeTypeNames[i]);
             }
             catch (IndexOutOfRangeException)
@@ -110,55 +143,13 @@ public class Get : MonoBehaviour
             }
 
         }
+        #endregion
 
     }
 
+    //Un metodo para hacer que la primera letra se ponga en mayuscula
     private string CapitalizeFirstLetter(string str)
     {
         return char.ToUpper(str[0]) + str.Substring(1);
     }
-
-    /*IEnumerator GetRequest(string uri)
-    {
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
-        {
-            // Request and wait for the desired page.
-            yield return webRequest.SendWebRequest();
-
-            string[] pages = uri.Split('/');
-            int page = pages.Length - 1;
-
-            //if (UnityWebRequest.result == UnityWebRequest.Result.ConnectionError)
-            if (webRequest.isNetworkError)
-            {
-                Debug.Log(pages[page] + ": Error: " + webRequest.error);
-            }
-            else
-            {
-                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
-            }
-        }
-    }*/
-
-    /*IEnumerator GetText()
-    {
-        using (UnityWebRequest www = UnityWebRequest.Get("http://www.my-server.com"))
-        {
-            yield return www.Send();
-
-            if (www.isNetworkError || www.isHttpError)
-            {
-                Debug.Log(www.error);
-            }
-            else
-            {
-                // Show results as text
-                Debug.Log(www.downloadHandler.text);
-
-                // Or retrieve results as binary data
-                byte[] results = www.downloadHandler.data;
-            }
-        }
-    }*/
-
 }
